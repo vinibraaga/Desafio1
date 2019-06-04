0c4a3a98d3e8ba1b3b3e596f609088dbd10e91c9
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-06-04 18:26:07 -0300

    atualizado

---
+++ pasta-projeto-node-site/app-banco.js
@@ -0,0 +1,46 @@
+var desenvolvimento = false;
+
+var configuracoes = {
+    producao: {
+        server: "jojocadastro.database.windows.net",
+        user: "vinibraaga",
+        password: "#Gf45747209809",
+        database: "jojo",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    },
+    desenvolvimento: {
+        server: "BASETESTE.database.windows.net",
+        user: "usuariotestes",
+        password: "senhatestes",
+        database: "BASETESTE",
+        options: {
+            encrypt: true
+        }
+    }
+}
+ 
+var sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+var perfil = desenvolvimento ? 'desenvolvimento' : 'producao';
+
+function conectar() {
+  sql.close();
+  return sql.connect(configuracoes[perfil])
+  // return new sql.ConnectionPool();  
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
